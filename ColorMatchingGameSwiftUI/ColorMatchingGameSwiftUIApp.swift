fc3cbe4f5f9d7091190b9419086c9d575685c66a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-21 09:45:00 +0300

    Initial Commit

---
+++ ColorMatchingGameSwiftUI/ColorMatchingGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchingGameSwiftUIApp.swift
+//  ColorMatchingGameSwiftUI
+//
+//  Created by ramil on 21.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatchingGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
